0b26ec42e209c6ca29c3aadc6cf33c5e3a24129b
tomaspeniak <tomas.peniak@gmail.com>
2023-12-19 13:36:04 +0100

    backend in progress

---
+++ E2E-Backend/E2EChatApp/Application/Extensions/ServiceAndRepositoryExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+namespace E2EChatApp.Application.Extensions;
+
+public static class ServiceAndRepositoryExtension {
+    public static IServiceCollection AddServicesAndRepositories(this IServiceCollection services) {
+        #region Services
+        // TODO: Add services here
+        #endregion
+        #region Repositories
+        // TODO: Add Repositories here
+        #endregion
+        
+        return services;
+    }
+}
--- E2E-Backend/E2EChatApp/Program.cs
+++ E2E-Backend/E2EChatApp/Program.cs
@@ -1,11 +1,40 @@
+using System.Data;
+using System.Text;
+using E2EChatApp.Application.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Npgsql;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Add auth
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+    var jwtConfig = builder.Configuration.GetSection("Jwt");
+    var key = Encoding.UTF8.GetBytes(jwtConfig.GetValue<string>("Key")
+                                     ?? throw new NullReferenceException("JWT key cannot be null"));
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtConfig.GetValue<string>("Issuer"),
+        ValidAudience = jwtConfig.GetValue<string>("Audience"),
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {});
+builder.Services.AddServicesAndRepositories();
+// Set up the DB connection
+builder.Services.AddTransient<IDbConnection>(new NpgsqlConnection(builder.Configuration.GetSection("ConnectionStrings").GetValue<string>("DefaultConnection")));
 
 var app = builder.Build();
 
