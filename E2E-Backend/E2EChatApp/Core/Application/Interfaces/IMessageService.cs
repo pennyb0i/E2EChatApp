62d7939b63ee5be1662df41e9c0f5492abe3f127
Tomáš Peniak <71640446+pennyb0i@users.noreply.github.com>
2023-12-30 14:02:46 +0100

    Message service tests

--- E2E-Backend/E2EChatApp/Core/Application/Interfaces/IMessageService.cs
+++ E2E-Backend/E2EChatApp/Core/Application/Interfaces/IMessageService.cs
@@ -4,7 +4,7 @@ namespace E2EChatApp.Core.Application.Interfaces;
 
 public interface IMessageService
 {
-    public Task<List<Message>> GetMessages(int firstUserId, int secondUserId);
+    public Task<List<Message>> GetMessages(int currentUserId, int otherUserId);
 
     Task SendMessage(int senderId, int receiverId, string content);
 }
